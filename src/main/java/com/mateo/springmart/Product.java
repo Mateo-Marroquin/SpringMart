5647b13d82289c6b768cca2942ebda76b8ea9456
Mateo Marroquin <mateo.marroquin.dev@outlook.com>
2026-06-12 14:02:01 -0600

    feat: create Product entity

--- src/main/java/com/mateo/springmart/Product.java
+++ src/main/java/com/mateo/springmart/Product.java
@@ -1,4 +1,65 @@
 package com.mateo.springmart;
 
-public record Product(long id, String name, double price, int quantity, String area) {
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private double price;
+    private int stock;
+    private String area;
+
+    public Product(){}
+
+    public Product(String name, double price, int stock, String area){
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+        this.area = area;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
 }
