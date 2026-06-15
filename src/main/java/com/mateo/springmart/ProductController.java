47c16c6a673aa1f40e23b6953f744e9443f2aefa
Mateo Marroquin <mateo.marroquin.dev@outlook.com>
2026-06-15 17:29:09 -0600

    feat: add post, put, delete methods

--- src/main/java/com/mateo/springmart/ProductController.java
+++ src/main/java/com/mateo/springmart/ProductController.java
@@ -14,12 +14,38 @@ public class ProductController {
     }
 
     @GetMapping
-    public List<Product> index(){
+    public List<Product> index() {
         return productRepository.findAll();
     }
 
     @GetMapping("/{id}")
-    public Product getProductById(@PathVariable long id){
-        return productRepository.findById(id).orElseThrow(()->new RuntimeException("Product with ID: " + id + " not found."));
+    public Product getProductById(@PathVariable long id) {
+        return productRepository.findById(id).orElseThrow(() -> new RuntimeException("Product with ID: " + id + " not found."));
+    }
+
+    @PostMapping
+    public Product newProduct(@RequestBody Product newProduct) {
+        return productRepository.save(newProduct);
+    }
+
+    @PutMapping("/{id}")
+    public Product replaceProduct(@RequestBody Product newProduct, @PathVariable Long id) {
+
+        return productRepository.findById(id)
+                .map(product -> {
+                    product.setName(newProduct.getName());
+                    product.setPrice(newProduct.getPrice());
+                    product.setStock(newProduct.getStock());
+                    product.setArea(newProduct.getArea());
+                    return productRepository.save(product);
+                })
+                .orElseGet(() -> {
+                    return productRepository.save(newProduct);
+                });
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Long id) {
+        productRepository.deleteById(id);
     }
 }
